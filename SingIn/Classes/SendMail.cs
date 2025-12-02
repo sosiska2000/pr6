2e5192d74ac780c77c66717e6031b398609ce04d
sosiska2000 <maratoleg2006@gmail.com>
2025-12-02 23:49:06 +0500

    Разработанный класс отправки сообщения на почту.KOmornyi

---
+++ SingIn/Classes/SendMail.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingIn.Classes
+{
+    public class SendMail
+    {
+        public static void SendMessage(string message, string to)
+        {
+            var SmtpClient = new SmtpClient()
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(),
+                EnableSsl = true
+            };
+            SmtpClient.Send("landaex@yandex.ru", to, "Проект RegIn", message);
+        }
+    }
+}
