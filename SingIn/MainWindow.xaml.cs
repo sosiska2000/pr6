3c741d07f2c3a32e426953ef749a30a6f90b81b5
sosiska2000 <maratoleg2006@gmail.com>
2025-12-02 23:28:03 +0500

    Разработанное окно MainWindow.Komornyi

--- SingIn/MainWindow.xaml.cs
+++ SingIn/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SingIn
+namespace RegIn
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
